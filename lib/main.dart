80cd541d796d354ca0dd700347e7d529a91ada78
James Krause <jameskrausethe3rd@gmail.com>
2025-02-16 18:05:44 -0600

    Some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/widgets.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'Grocery List App',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
@@ -25,21 +26,55 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+class GroceryItem {
+  String _label;
+  bool _isBought;
+  String _objectID;
 
-  void getNext() {
-    current = WordPair.random();
+  GroceryItem({required String label, bool isBought = false})
+      : _label = label,
+        _isBought = isBought,
+        _objectID = Uuid().v4(); // Generates a unique ID using UUID
+
+  // Getter for label
+  String get label => _label;
+
+  // Setter for label
+  set label(String newLabel) {
+    _label = newLabel;
+  }
+
+  // Getter for isBought
+  bool get isBought => _isBought;
+
+  // Setter for isBought
+  set isBought(bool status) {
+    _isBought = status;
+  }
+
+  // Getter for objectID
+  String get objectID => _objectID;
+}
+
+class MyAppState extends ChangeNotifier {
+  final Map<String, GroceryItem> items = {};
+
+  void addItem(String item) {
+    GroceryItem newItem = GroceryItem(label: item);
+    items[newItem._objectID] = newItem;
     notifyListeners();
   }
 
-  var favorites = <WordPair>[];
+  void removeItem(String itemID) {
+    items.remove(itemID);
+    notifyListeners();
+  }
 
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
+  void toggleBought(String itemID) {
+    if (items[itemID]?._isBought == true) {
+      items[itemID]?._isBought = false;
     } else {
-      favorites.add(current);
+      items[itemID]?._isBought = true;
     }
     notifyListeners();
   }
@@ -58,7 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
     Widget page;
     switch (selectedIndex) {
       case 0:
-        page = GeneratorPage();
+        page = ListPage();
         break;
       case 1:
         page = FavoritesPage();
@@ -76,8 +111,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 extended: constraints.maxWidth >= 600,
                 destinations: [
                   NavigationRailDestination(
-                    icon: Icon(Icons.home),
-                    label: Text('Home'),
+                    icon: Icon(Icons.list),
+                    label: Text('List'),
                   ),
                   NavigationRailDestination(
                     icon: Icon(Icons.favorite),
@@ -105,46 +140,80 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class GeneratorPage extends StatelessWidget {
+class ListPage extends StatefulWidget {
+  @override
+  State<ListPage> createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  final TextEditingController _controller = TextEditingController();
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
+
+  void _addItem(BuildContext context) {
+    var appState = context.read<MyAppState>();
+    if (_controller.text.isNotEmpty) {
+      appState.addItem(_controller.text);
+      _controller.clear();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    var pair = appState.current;
 
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
+    // Filter the Map to only include items where isBought is false
+    List<MapEntry<String, GroceryItem>> groceryList =
+        appState.items.entries.where((entry) => !entry.value.isBought).toList();
 
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            key: _listKey,
+            itemCount: groceryList.length,
+            itemBuilder: (context, index) {
+              final item = groceryList[index];
+              return ListTile(
+                title: Text(item.value._label),
+                leading: Checkbox(
+                  value: item.value._isBought,
+                  onChanged: (_) {
+                    appState.toggleBought(item.value._objectID);
+                  },
+                ),
+                trailing: IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    appState.removeItem(item.value._objectID);
+                  },
+                ),
+              );
+            },
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
             children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
+              Expanded(
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    labelText: 'Enter an item',
+                    border: OutlineInputBorder(),
+                  ),
+                  onSubmitted: (_) => _addItem(context),
+                ),
               ),
               SizedBox(width: 10),
               ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
+                onPressed: () => _addItem(context),
+                child: Icon(Icons.add),
               ),
             ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
@@ -154,53 +223,36 @@ class FavoritesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
-    if (appState.favorites.isEmpty) {
+    // Filter the Map to only include items where isBought is true
+    List<MapEntry<String, GroceryItem>> groceryList =
+        appState.items.entries.where((entry) => entry.value.isBought).toList();
+
+    if (groceryList.isEmpty) {
       return Center(
-        child: Text('No favorites yet.'),
+        child: Text('No items.'),
       );
     }
 
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
+    return ListView.builder(
+      itemCount: groceryList.length,
+      itemBuilder: (context, index) {
+        final item = groceryList[index];
+        return ListTile(
+          title: Text(item.value._label),
+          leading: Checkbox(
+            value: item.value._isBought,
+            onChanged: (_) {
+              appState.toggleBought(item.value._objectID);
+            },
           ),
-      ],
-    );
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-
-    return Card(
-      color: theme.colorScheme.primary,
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase,
-            style: style, semanticsLabel: "${pair.first} ${pair.second}"),
-      ),
+          trailing: IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              appState.removeItem(item.value._objectID);
+            },
+          ),
+        );
+      },
     );
   }
 }
